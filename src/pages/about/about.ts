df8d1c29b76a2d7766f8afa91293aedfdf5f0ca0
arthurOcg <arthur.lima.oliveira@ccc.ufcg.edu.br>
2018-09-09 23:41:19 -0300

    Adicionado botões em about, um de alerta e outro de actionList

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ActionSheetController, AlertController } from 'ionic-angular';
+import { ContactPage } from '../contact/contact';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +8,47 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  contactPage = ContactPage;
+
+  constructor(public navCtrl: NavController, public actionSheetController: ActionSheetController, public alertController: AlertController ) {
 
   }
 
+  mostrarActionSheet() {
+    const actionSheet = this.actionSheetController.create({
+      title: 'Lista de Ações',
+      buttons: [
+        {
+          text: 'Opção 1',
+          handler: () => {
+            console.log('Opção 1');
+          }
+        },{
+          text: 'Opção 2',
+          handler: () => {
+            console.log('Opção 2');
+          }
+        },{
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelar');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+
+  Alerta() {
+    const alert = this.alertController.create({
+      title: 'Titulo!',
+      subTitle: 'Subtitulo!',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
 }
+
